1ec94a62fb9c89571d97cb8d19fb32fa8e41aa49
EC2 Default User <ec2-user@ip-172-31-2-127.eu-north-1.compute.internal>
2024-04-07 09:59:21 +0000

    appspec.yml and scripts setup

--- api/tmp/scripts/after_install.sh
+++ api/tmp/scripts/after_install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-echo 'run after_install.sh: ' >> /home/ec2-user/acebook/api/deploy.log
+echo 'run after_install.sh:' >> /home/ec2-user/acebook/api/deploy.log
 
-echo 'cd /home/ec2-user/myrepo' >> /home/ec2-user/acebook/api/deploy.log
-cd /home/ec2-user/myrepo >> /home/ec2-user/acebook/api/deploy.log
+echo 'cd /home/ec2-user/acebook/api' >> /home/ec2-user/acebook/api/deploy.log
+cd '/home/ec2-user/acebook/api' >> /home/ec2-user/acebook/api/deploy.log
 
 echo 'npm install' >> /home/ec2-user/acebook/api/deploy.log 
 npm install >> /home/ec2-user/acebook/api/deploy.log
--- api/tmp/scripts/application_start.sh
+++ api/tmp/scripts/application_start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-echo 'run application_start.sh: ' >> /home/ec2-user/acebook/api/deploy.log
+echo 'run application_start.sh:' >> /home/ec2-user/acebook/api/deploy.log
 # nodejs-app is the same name as stored in pm2 process
 echo 'restarting the service - acebook-server' >> /home/ec2-user/acebook/api/deploy.log
 systemctl restart acebook-server >> /home/ec2-user/acebook/api/deploy.log
+echo 'restarting the service - nginx' >> /home/ec2-user/acebook/api/deploy.log
+systemctl restart nginx >> /home/ec2-user/acebook/api/deploy.log
